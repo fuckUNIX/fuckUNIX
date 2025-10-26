b5b152faceb12bec20ecead59430a062a3c52f39
ByteDev <noreply@local>
2025-10-26 14:04:53 +0100

    Fix bugs and add klaunch to the kernel

---
+++ apps/terminal.c
@@ -0,0 +1,12 @@
+#include "../drivers/keyboard.h"
+#include "../drivers/display.h"
+
+#include "apps.h"
+
+#define terminal_banner "fuckSH 1.0: truly a masterpiece\n by ByteDev"
+
+// runs the terminal app
+void terminal_init() {
+    print_string(terminal_banner);
+    print_string("\n>");
+}
